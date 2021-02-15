1e980113b88aed870072997fe24720badc670621
Devin Neal <devin@eulertour.com>
2021-02-14 17:44:20 -0800

    Update 2021_02_14_valentines.py

--- 2021_02_14_valentines.py
+++ 2021_02_14_valentines.py
@@ -369,23 +369,3 @@ class ValentineScene(GraphScene, MovingCameraScene):
         )
 
         self.wait()
-
-
-class FunctionPlotWithLabelledYAxis(GraphScene):
-    def __init__(self, **kwargs):
-        GraphScene.__init__(
-            self,
-            y_min=0,
-            y_max=100,
-            y_axis_config={"tick_frequency": 10},
-            y_labeled_nums=np.arange(0, 100, 10),
-            **kwargs
-        )
-
-    def construct(self):
-        self.setup_axes()
-        dot = Dot().move_to(self.coords_to_point(PI / 2, 20))
-        func_graph = self.get_graph(lambda x: 20 * np.sin(x))
-        self.add(dot, func_graph)
-        func_graph.add_updater(lambda mob, dt: mob)
-        self.play(func_graph.animate.shift(UP))
